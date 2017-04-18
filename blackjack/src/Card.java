ebb95dd5dbec1994a212547d9f75bfa94e3c958c
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-04-18 10:29:27 +0200

    Creates Card class

---
+++ blackjack/src/Card.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Adam on 2017. 04. 18..
+ */
+public class Card {
+  private String value;
+  private String color;
+
+  public Card(String value, String color) {
+    this.value = value;
+    this.color = color;
+  }
+}
