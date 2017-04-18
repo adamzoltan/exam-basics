57d2d3004b22f46842856ea3d9b030ac5f9527e1
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-04-18 09:56:34 +0200

    Creates test methods

--- oddavg/src/OddAvg.java
+++ oddavg/src/OddAvg.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class OddAvg {
@@ -5,10 +6,12 @@ public class OddAvg {
     // Create a function called `oddAverage` that takes a list of numbers as parameter
     // and returns the average value of the odd numbers in the list
     // Create basic unit tests for it with at least 3 different test cases
+
   }
-  public int oddAverage (List<Integer> nums) {
+
+  public static int oddAverage(List<Integer> nums) {
     int average = 0;
-    int counter =0;
+    int counter = 0;
     for (int i = 0; i < nums.size(); i++) {
       if (nums.get(i) % 2 != 0) {
         average = average + nums.get(i);
@@ -17,4 +20,5 @@ public class OddAvg {
     }
     return average / counter;
   }
+
 }
